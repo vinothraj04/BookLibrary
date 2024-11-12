547b5c923abbba460e39f335d5dc60a7a085a489
vinothraj04 <vinothrajb08@gmail.com>
2024-11-12 22:03:57 +0530

    updated

--- src/Loginpage.jsx
+++ src/Loginpage.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import './Login.css'; // Import the CSS file here
 
-const Loginpage = ({ onLogin }) => {
+const loginpage = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -33,4 +33,4 @@ const Loginpage = ({ onLogin }) => {
   );
 };
 
-export default Loginpage;
+export default loginpage;
